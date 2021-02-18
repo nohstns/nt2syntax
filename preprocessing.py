bc3a1b5a2bd7c87fd59c5aaff28a280ddf56c276
Nafal <nafal.ossandon@gmail.com>
2021-02-18 15:34:17 +0100

    Script clean-up: main() definition
    Making the script executable; definition of the main() function and
    expanding function documentation.

--- preprocessing.py
+++ preprocessing.py
@@ -15,7 +15,16 @@ data = pd.read_csv('NT2schrijven.csv', sep = ';', index_col = 0)
 data = data['typedText'].to_frame(name = 'TypedText')
 data['Evaluation'] = np.nan
 
+
+#---------------------------------------------------#
+#   Preprocessing functions definition to fit       #
+#   T-scan's formatting requirements.               #
+#---------------------------------------------------#
+
 def replace_parenthesis(text):
+    '''
+    Replaces all squared brackets with round brackets.
+    '''
     for char in text:
         if char == '[':
             text = text.replace('[', '(')
@@ -23,35 +32,54 @@ def replace_parenthesis(text):
             text = text.replace(']', ')')
     return text
 
-# Generate new version of the text without spelling errors
-def spellcheck():
+def spellcheck(): # Function for tool testing purposes; not analysis
+    '''
+    Corrects spelling mistakes in the texts.
+    '''
     pass
 
 
-# Apply preprocessing functions on the dataset
-actions = [replace_parenthesis]
 
-for action in actions:
-    data['TypedText'] = data.TypedText.apply(action)
 
-print(data.head(10))
+def apply_preprocessing():
+    '''
+    Apply preprocessing functions on the dataset
+    '''
+    actions = [replace_parenthesis]
+
+    for action in actions:
+        data['TypedText'] = data.TypedText.apply(action)
+
 
 #---------------------------------------------------#
-#   Generate two files with the preprocessed data;  #
-#   a *.txt for use with T-scan and a *.csv         #
-#   containing the participant ID linked to the     #
-#   actual text. Line order between the two files   #
-#   is the same.                                    #
+#   File generation                                 #
 #---------------------------------------------------#
 
-#data['TypedText'].to_csv('dataset_v1.txt', sep = ' ', index = False, header = False)
 
-# Write *.txt
-with open('dataset.txt', 'w', encoding = 'utf-8') as f:
-    for text in data['TypedText']:
-        f.write(f'{text}\n')
+def generate_txt():
+    '''
+    Generates a *.txt file with the texts preprocessed
+    for analysis with T-scan.
+    '''
+    with open('dataset.txt', 'w', encoding = 'utf-8') as f:
+        for text in data['TypedText']:
+            f.write(f'{text}\n')
+
+def generate_csv():
+    '''
+    Generates a *.csv file with the texts preprocessed
+    for analysis with T-scan, their corresponding evaluation
+    and the participants' ID.
+    '''
+    data.to_csv(r'dataset_v2.csv', sep = ',')
 
 
-#data.to_csv(r'dataset_v2.csv', sep = ',')
+#---------------------------------------------------#
+#   main() definition                               #
+#---------------------------------------------------#
 
-print('DONE')
+def main():
+    apply_preprocessing()
+    generate_txt()
+
+main()

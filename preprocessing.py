bd927cff5f80e64259da9419063bbb732abf9724
Nafal <nafal.ossandon@gmail.com>
2021-02-25 19:53:52 +0100

    Add sentence_limit_fix to correct encoding corruption of sentence boundaries, add function to generate separate *.txt files for each text sample

--- preprocessing.py
+++ preprocessing.py
@@ -32,20 +32,24 @@ def replace_parenthesis(text):
             text = text.replace(']', ')')
     return text
 
-def spellcheck(): # Function for tool testing purposes; not analysis
+def sentence_limit_fix(text):
     '''
-    Corrects spelling mistakes in the texts.
+    Fixes sentence delimitation where the space between
+    sentences has been lost, whether a period is used
+    or not. Splits
+    joined sentences, ends the first one with a
+    period if not present and adds a space between
+    the sentences.
     '''
-    pass
-
-
-
+    pattern = re.compile(r'(?<=[a-z])(\.|\?|\!)*(?<! )((?=[A-Z])|$)')
+    corrected = re.sub(pattern, '. ', text)
+    return corrected
 
 def apply_preprocessing():
     '''
     Apply preprocessing functions on the dataset
     '''
-    actions = [replace_parenthesis]
+    actions = [replace_parenthesis, sentence_limit_fix]
 
     for action in actions:
         data['TypedText'] = data.TypedText.apply(action)
@@ -65,6 +69,22 @@ def generate_txt():
         for text in data['TypedText']:
             f.write(f'{text}\n')
 
+def split_text_files():
+    '''
+    Generates an individual *.txt file for every preprocessed
+    text for analysis with T-scan.
+    '''
+    try:
+        os.mkdir('preprocessed')
+    except FileExistsError:
+        pass
+
+    cnt = 0
+    for text in data['TypedText']:
+        with open(f'preprocessed/text_{cnt}.txt', 'w', encoding = 'utf-8') as f:
+            f.write(text)
+        cnt += 1
+
 def generate_csv():
     '''
     Generates a *.csv file with the texts preprocessed
@@ -80,6 +100,5 @@ def generate_csv():
 
 def main():
     apply_preprocessing()
-    generate_txt()
-
+    split_text_files()
 main()

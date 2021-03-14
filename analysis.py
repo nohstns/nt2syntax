7ef565089f7aabcc7871a8049dd2dd62cbcda3d5
Nafal <nafal.ossandon@gmail.com>
2021-03-14 20:08:05 +0100

    Add get_synt_feat() function
    Similar to the function in this version of parsing.py, but this time working with the output of Alpino
    and with the dependency tags instead of the POS tags.
    
    Extracts the number of vp, np and pp in each text and returns a tuple with a list of all the phrase
    instances (not only these three), then the count of vp, np and pp.
    
    To be corrected: Does not recognise vps since they are not tagged as such.

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@
 
 import os
 import pandas as pd
+import regex as re
+import xml.etree.ElementTree as ET
 
 #---------------------------------------------------#
 #   Open T-scan output from different analysis' pov #
@@ -16,6 +18,47 @@ import pandas as pd
 df_doc = pd.read_csv('total.doc.csv', sep = ',', index_col = 0)
 df_sen = pd.read_csv('total.sen.csv', sep = ',', index_col = 0)
 
-def check_parsing(df):
-    if 1 in df.Alpino_status.values:
-        print('WARNING: Alpino parsing was unsuccessful for one or more sentences.')
+
+#---------------------------------------------------#
+#   Open Alpino output from outputfolder xml        #
+#   where each text has its own folder with a .xml  #
+#   output file, e.g.                               #
+#                                                   #
+#   xml > text_0.txt > 1.xml                        #
+#---------------------------------------------------#
+
+# Extract syntactic features
+
+def get_synt_feat(root):
+    '''
+    Extracts the values of the <node lcat> attribute for the entire text
+    and stores them in a list.
+    This node corresponds to the syntactic feature of each phrase.
+    '''
+    vp = []
+    np = []
+    pp = []
+    all_p = [] # Variable for testing purposes
+
+    for element in root.iter('*'):
+        if element.tag == 'node':
+            xp = element.get('lcat')
+            if xp != None:
+                all_p.append(xp)
+            if xp == 'vp':
+                vp.append(xp)
+            if xp == 'np':
+                np.append(xp)
+            if xp == 'pp':
+                pp.append(xp)
+
+    return(all_p, len(vp), len(np), len(pp))
+
+
+# Define number of texts to be analysed
+n = len([f for f in os.listdir('./xml') if f.endswith('.txt')])
+
+for text in range(0, n):
+    root = ET.parse(f'./xml/text_{text}.txt/1.xml').getroot()
+    print(text, '\t', get_synt_feat_(root))
+    print('\n\n\n')

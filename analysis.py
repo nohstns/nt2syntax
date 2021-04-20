126abc50b106f86236eafc9ceb0cb64ab240086a
Nafal <nafal.ossandon@gmail.com>
2021-04-20 17:02:14 +0200

    Usage optimalization
    Added possibility to select the preprocessed dataset to be analysed from the
    command line when calling the script.

--- analysis.py
+++ analysis.py
@@ -1,27 +1,75 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import pandas as pd
 from features import *
 
-#---------------------------------------------------#
-#   Open dataset and generate dataframe to store    #
-#   the extracted features.                         #
-#---------------------------------------------------#
+USAGE = f"Usage: python {sys.argv[0]} [--help | -h] | [<preprocessed dataset.csv filename> <new dataset label>]"
 
-df = pd.read_csv('dataset.csv', sep = ',')
+def main():
 
-# Add column with the text number for its use in the feature extraction functions
+    #---------------------------------------------------#
+    #   Read command line arguments                     #
+    #---------------------------------------------------#
 
-df['text_n'] = df.index
+    if len(sys.argv) == 3:
+        if sys.argv[1] in os.listdir():
+            script, filename, dataset_label = sys.argv
+            dataset_label = '_' + dataset_label
+        else:
+            print('Dataset import error')
+            raise SystemExit(USAGE)
 
-# Add feature columns and fill them with empty values
-for index in indices:
-    df[index] = np.nan
+    elif len(sys.argv) == 2:
+        script = sys.argv[0]
+
+        if sys.argv[1] == '--help' or sys.argv[1] == '-h':
+            print(f'''
+            {USAGE}
+
+            TO WRITE: HELP
+
+            An optional second argument can be used to keep multiple outputs separated.
+            Not specifying a label can lead to overwriting files.
+
+            e.g. python {script} dataset.csv v1
+            ''')
+            sys.exit()
+
+        if sys.argv[1] in os.listdir():
+            filename = sys.argv[1]
+            dataset_label = ''
+        else:
+            print('Dataset import error')
+            raise SystemExit(USAGE)
+
+    #---------------------------------------------------#
+    #   Open dataset and generate dataframe to store    #
+    #   the extracted features.                         #
+    #---------------------------------------------------#
+
+    df = pd.read_csv(filename, sep = ',')
+
+    # Add column with the text number for its use in the feature extraction functions
+
+    df['text_n'] = df.index
+
+    # Add feature columns and fill them with empty values
+    for index in indices:
+        df[index] = np.nan
 
 
-for index in indices:
-    apply_index_getter(df, index)
+    for index in indices:
+        apply_index_getter(df, index, level = 'doc')
 
-    
-df.to_csv(r'dataset_features.csv', sep = ',')
+
+    df.to_csv(f'vectorized_{filename}{dataset_label}.doc.csv', sep = ',')
+
+    for index in indices:
+        apply_index_getter(df, index, level = 'sen')
+
+    df.to_csv(f'vectorized_{filename}{dataset_label}.sen.csv', sep = ',')
+
+if __name__ == "__main__":
+   main()
